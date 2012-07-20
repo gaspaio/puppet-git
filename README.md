793655f0ff3b5e9c69639e456d079c13d53b9772
Rodolfo Ripado <ggaspaio@gmail.com>
2012-07-20 09:59:48 +0200

    initial commit

--- README.md
+++ README.md
@@ -2,3 +2,17 @@ puppet-git
 ==========
 
 Puppet module to handle Git repositories
+
+Example
+-----
+
+
+    class{'git':
+      username =>  'MYNAME',
+      usermail =>  'MYMAIL'
+    }
+
+    git::clone {'repo_name':
+      source    => 'URL',
+      localtree => 'DIRECTORY',
+    }
---
+++ manifests/clone.pp
@@ -0,0 +1,32 @@
+# Inspired by https://github.com/camptocamp/puppet-git
+
+define git::clone(
+  $source,
+  $localtree,
+  $real_name = false,
+  $branch = false) {
+
+ if $real_name {
+    $_name = $real_name
+  }
+  else {
+    $_name = $name
+  }
+
+  exec { "git_clone_exec_$localtree/$_name":
+    cwd => $localtree,
+    command => "git clone $source $_name",
+    creates => "$localtree/$_name/.git/"
+  }
+
+  case $branch {
+    false: {}
+    default: {
+      exec { "git_clone_checkout_$branch_$localtree/$_name":
+        cwd => "$localtree/$_name",
+        command => "git checkout --track -b $branch origin/$branch",
+        creates => "$localtree/$_name/.git/refs/heads/$branch"
+      }
+    }
+  }
+}
